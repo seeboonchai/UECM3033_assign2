0bfabe095996e61d1d8e66003c8b3f548163b58e
seeboonchai <1300400@1utar.edu.my>
2016-03-11 05:24:40 +0800

    add

--- task1.py
+++ task1.py
@@ -1,41 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 22:29:08 2016
+
+@author: Edric
+"""
+
 import numpy as np
-#Your optional code here
-#You can import some modules or create additional functions
 
+
+ #function of lu that perform finding  
 def lu(A, b):
-    sol = []
-    # Edit here to implement your code
-    return list(sol)
+    
+    n = len(A)
+    for k in range(0, n-1):
+        for i in range(k+1, n):
+            if A[i,k] != 0.0:
+                lam = A[i,k] / A[k,k]
+                A[i, k+1:n] = A[i, k+1:n] - lam * A[k, k+1:n]
+                A[i, k] = lam
+    
+    for k in range(1,n):
+        b[k] = b[k] - np.dot(A[k,0:k], b[0:k])
+    b[n-1]=b[n-1]/A[n-1, n-1]
+    for k in range(n-2, -1, -1):
+        b[k] = (b[k] - np.dot(A[k,k+1:n], b[k+1:n]))/A[k,k]
 
+    return list(b)
+    
+#function of sor that perform finding
 def sor(A, b):
-    sol = []
-    # Edit here to implement your code
-    return list(sol)
+    
+    Diag = np.diag(np.diag(A))
+    Ltmatrix = np.tril(-A,-1)
+    Utmatrix = A-Ltmatrix-Diag
+   
+   
+    spars = max(abs(np.linalg.eigvals(np.dot(np.linalg.inv(Diag),(Ltmatrix+Utmatrix)))))
 
+    w_omega = 2*(1 - np.sqrt(1 - ((spars)**2)))/((spars)**2)
+    x = np.zeros_like(b)
+    for itr in range(10):
+        for i in range(len(b)):
+            sums = np.dot( A[i,:], x )
+            x[i-1] = x[i-1] + w_omega*(b[i-1]-sums)/A[i-1,i-1]
+        
+    return list(np.array([x[1],x[2],x[3]]))
+
+#function to solve using sor or lu depend on condition
 def solve(A, b):
-    condition = True # State and implement your condition here
-    if condition:
+    
+    if (np.all(A-A.T== 0) & np.all(np.linalg.eigvals(A)) > 0):
+         print('Solve by sor(A,b)')
+         return sor(A,b)
+    else:
+       
         print('Solve by lu(A,b)')
         return lu(A,b)
-    else:
-        print('Solve by sor(A,b)')
-        return sor(A,b)
 
 if __name__ == "__main__":
     ## import checker
     ## checker.test(lu, sor, solve)
 
-    A = [[2,1,6], [8,3,2], [1,5,1]]
-    b = [9, 13, 7]
-    sol = solve(A,b)
-    print(sol)
+    A = np.array([[2.0,1.0,6.0], [8.0,3.0,2.0], [1.0,5.0,1.0]])
+    b = np.array([9.0, 13.0, 7.0])
     
-    A = [[6566, -5202, -4040, -5224, 1420, 6229],
-         [4104, 7449, -2518, -4588,-8841, 4040],
-         [5266,-4008,6803, -4702, 1240, 5060],
-         [-9306, 7213,5723, 7961, -1981,-8834],
-         [-3782, 3840, 2464, -8389, 9781,-3334],
-         [-6903, 5610, 4306, 5548, -1380, 3539.]]
-    b = [ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906]
-    sol = solve(A,b)
-    print(sol)
+    print(solve(A,b))
+    
+    A = np.array([[6566, -5202, -4040, -5224, 1420, 6229],
+                 [4104, 7449, -2518, -4588,-8841, 4040],
+                [5266,-4008,6803, -4702, 1240, 5060],
+                [-9306, 7213,5723, 7961, -1981,-8834],
+                [-3782, 3840, 2464, -8389, 9781,-3334],
+                  [-6903, 5610, 4306, 5548, -1380, 3539.]]).astype(float)
+    b = np.array([ 17603,  -63286,   56563,  -26523.5, 103396.5, -27906]).astype(float)
+   # sol = solve(A,b)
+    
--- task2.py
+++ task2.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
+#this is image file loading
 img=mpimg.imread('camera.tiff')
 [r,g,b] = [img[:,:,i] for i in range(3)]
 
@@ -17,3 +18,72 @@ ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
+
+#singular value decomposition
+U_red, Sig_red, V_red = np.linalg.svd(r,full_matrices = True)
+U_green, Sig_green, V_green = np.linalg.svd(g,full_matrices = True)
+U_blue, Sig_blue, V_blue = np.linalg.svd(b,full_matrices = True)
+n_sig = np.count_nonzero(Sig_red)
+print("The number of the non zero element for ∑  will be  : ", n_sig)
+n_data, dim = r.shape
+
+# Compression for Lower Resolution picture
+#red image
+Sig_RED_30=Sig_red
+Sig_RED_30[30::] = 0
+RED_30 = np.matrix(U_red) * np.diag(Sig_RED_30) * np.matrix(V_red[:n_data, :])
+
+#green image
+Sig_GREEN_30=Sig_green
+Sig_GREEN_30[30::] = 0
+green_30 = np.matrix(U_green) * np.diag(Sig_GREEN_30) * np.matrix(V_green[:n_data, :])
+
+#blue image
+Sig_BLUE_30=Sig_blue
+Sig_BLUE_30[30::] = 0
+blue_30 = np.matrix(U_blue) * np.diag(Sig_BLUE_30) * np.matrix(V_blue[:n_data, :])
+
+#display red, green, blue image.
+fig = plt.figure(1)
+ax1 = fig.add_subplot(3,1,1)
+ax2 = fig.add_subplot(3,1,2)
+ax3 = fig.add_subplot(3,1,3)
+ax1.imshow(RED_30, cmap = 'Reds')
+ax2.imshow(green_30, cmap = 'Greens')
+ax3.imshow(blue_30, cmap = 'Blues')
+fig = plt.gcf()
+fig.set_size_inches(15,15)
+plt.suptitle('Lower Resolution picture, size=30')
+plt.show()
+
+# Compression for Higher Resolution picture
+#red image
+U_red, Sig_red, V_red = np.linalg.svd(r,full_matrices = True)
+Sig_red_200=Sig_red
+Sig_red_200[200::] = 0
+red_200 = np.matrix(U_red) * np.diag(Sig_red_200) * np.matrix(V_red[:n_data, :])
+
+#green image
+U_green, Sig_green, V_green = np.linalg.svd(g,full_matrices = True)
+Sig_green_200=Sig_green
+Sig_green_200[200::] = 0
+green_200 = np.matrix(U_green) * np.diag(Sig_green_200) * np.matrix(V_green[:n_data, :])
+
+#blue image
+U_blue, Sig_blue, V_blue = np.linalg.svd(b,full_matrices = True)
+Sig_blue_200=Sig_blue
+Sig_blue_200[200::] = 0
+blue_200 = np.matrix(U_blue) * np.diag(Sig_blue_200) * np.matrix(V_blue[:n_data, :])
+
+#display red, green, blue image.
+fig = plt.figure(1)
+ax1 = fig.add_subplot(3,1,1)
+ax2 = fig.add_subplot(3,1,2)
+ax3 = fig.add_subplot(3,1,3)
+ax1.imshow(red_200, cmap = 'Reds')
+ax2.imshow(green_200, cmap = 'Greens')
+ax3.imshow(blue_200, cmap = 'Blues')
+fig = plt.gcf()
+fig.set_size_inches(15,15)
+plt.suptitle('Higher Resolution picture, size=200')
+plt.show()
