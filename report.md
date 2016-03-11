ec0bb1b068431eb20857b63dc6da6e98fba3871b
seeboonchai <1300400@1utar.edu.my>
2016-03-11 23:38:30 +0800

    collection

--- report.md
+++ report.md
@@ -19,7 +19,6 @@ If the matrix have all positive eigenvalues and it is symmetric,after perform ca
 Explain how you implement your `task1.py` here.
 
 For task 1, inside the lu method first i try define the range and set some property  for matrix A and b in order to perform lu factorisation so that can get accurate result for b.Next, for sor method i define L,u,d which is lower,upper  triangular matrices and diagonal matrix, besides i define also a sparse matrix for substitution purpose to get omega value, with this omega value then we can get the actual formula for sor method for computation of linear system.inside solve method i try set a logical statement to make sure can get convergence result but based on criteria of sor and lu method.After run the code,i found that lu method get convergence as expected.
----------------------------------------------------------
 
 ## Task 2 -- SVD method and image compression
 
@@ -31,7 +30,13 @@ There are 800 non zero element in $Sigma$
 
 Put here your lower and better resolution pictures. Explain how you generate
 these pictures from `task2.py`.
-![cameratransform.tiff](cameratransform.tiff)
+this is my lower resolution image, n=30
+
+![lower.tiff](lower.tiff)
+
+this is my high resolution image, n=200
+
+![high.tiff](high.tiff)
 
  From beginning, i created a  singular value decomposition function which also known as svd function.Next,i read the image which is then compressed by keeping the first 30 non zero elements as $\Sigma$ and then  i set other non zero elements to zero,all this is first for red matrix. After that i repeat the same process by using green and blue matrix.From this task, i put in n=30 for lower resolution and n=200 for better resolution.
 
@@ -40,6 +45,6 @@ these pictures from `task2.py`.
 What is a sparse matrix?
 
 In numerical analysis, a sparse matrix is a matrix in which most of the elements are zero
------------------------------------
+
 
 <sup>last modified: change your date here</sup>
--- task2.py
+++ task2.py
@@ -1,103 +1,82 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import scipy.linalg as sp
 
-#this is image file loading
+#loading image
 img=mpimg.imread('camera.tiff')
-[r,g,b] = [img[:,:,i] for i in range(3)]
+img1=mpimg.imread('camera.tiff')
+[r,g,b] = [img1[:,:,i] for i in range(3)]
 
 
-fig = plt.figure(1)
-ax1 = fig.add_subplot(2,2,1)
-ax2 = fig.add_subplot(2,2,2)
-ax3 = fig.add_subplot(2,2,3)
-ax4 = fig.add_subplot(2,2,4)
+
+
+U_red,sigma_red,V_red = sp.svd(r,False,True,False,True)
+U_green,sigma_green,V_green = sp.svd(g,False,True,False,True)
+U_blue,sigma_blue,V_blue = sp.svd(b,False,True,False,True)
+
+#Sigma is convert from vector to  a diagonal matrix
+sigma_red = np.diag(sigma_red)
+sigma_green = np.diag(sigma_green)
+sigma_blue = np.diag(sigma_blue)
+
+# Compress the Lower resolution picture
+sigma_RED = np.zeros_like(sigma_red)
+sigma_GREEN = np.zeros_like(sigma_green)
+sigma_BLUE = np.zeros_like(sigma_blue)
+
+# make sure the first 30 none zero element are  Sigma
+sigma_RED[0:30] = sigma_red[0:30]
+sigma_GREEN[0:30] = sigma_green[0:30]
+sigma_BLUE[0:30] = sigma_blue[0:30]
+
+RED   = U_red.dot(sigma_RED).dot(V_red)
+GREEN = U_green.dot(sigma_GREEN).dot(V_green)
+BLUE  = U_blue.dot(sigma_BLUE).dot(V_blue)
+
+img[:,:,0] = RED
+img[:,:,1] = GREEN
+img[:,:,2] = BLUE
+
+figure = plt.figure(1)
+ax1 = figure.add_subplot(2,2,1)
+ax2 = figure.add_subplot(2,2,2)
+ax3 = figure.add_subplot(2,2,3)
+ax4 = figure.add_subplot(2,2,4)
 ax1.imshow(img)
-ax2.imshow(r, cmap = 'Reds')
-ax3.imshow(g, cmap = 'Greens')
-ax4.imshow(b, cmap = 'Blues')
-plt.show()
-
-
-#singular value decomposition
-U_red, sigma_red, V_red = np.linalg.svd(r,full_matrices = True)
-U_green, sigma_g, V_green = np.linalg.svd(g,full_matrices = True)
-U_blue, sigma_b, V_blue = np.linalg.svd(b,full_matrices = True)
-n_sigma = np.count_nonzero(sigma_red)
-print("non zero element for sigma of r, g, b : ", n_sigma)
-num_data, dim = r.shape
-
-# Compress-Lower Resolution picture
-#red image
-sigma_red_30=np.zeros_like(sigma_red)
-sigma_red_30[:30] = sigma_red[:30]
-r_30 = np.matrix(U_red) * np.diag(sigma_red_30) * np.matrix(V_red[:num_data, :])
-
-#green image
-sigma_green_30=np.zeros_like(sigma_g)
-sigma_green_30[:30] = sigma_g[:30]
-g_30 = np.matrix(U_green) * np.diag(sigma_green_30) * np.matrix(V_green[:num_data, :])
-
-#blue image
-sigma_blue_30=np.zeros_like(sigma_b)
-sigma_blue_30[:30] = sigma_b[:30]
-b_30 = np.matrix(U_blue) * np.diag(sigma_blue_30) * np.matrix(V_blue[:num_data, :])
-
-#compress  the imageinto color image
-low_img = img
-low_img[:,:,0] = r_30
-low_img[:,:,1] = g_30
-low_img[:,:,2] = b_30
-
-#display red, green, blue image.
-fig = plt.figure(1)
-ax1 = fig.add_subplot(2,2,1)
-ax2 = fig.add_subplot(2,2,2)
-ax3 = fig.add_subplot(2,2,3)
-ax4 = fig.add_subplot(2,2,4)
-ax1.imshow(r_30, cmap = 'Reds')
-ax2.imshow(g_30, cmap = 'Greens')
-ax3.imshow(b_30, cmap = 'Blues')
-ax4.imshow(low_img)
-fig = plt.gcf()
-fig.set_size_inches(15,15)
+ax2.imshow(RED, cmap = 'Reds')
+ax3.imshow(GREEN, cmap = 'Greens')
+ax4.imshow(BLUE, cmap = 'Blues')
 plt.suptitle('Lower Resolution, size=30')
 plt.show()
 
-# Compress-Higher Resolution picture
-#red image
-sigma_red_200=np.zeros_like(sigma_red)
-sigma_red_200[:200] = sigma_red[:200]
-red_200 = np.matrix(U_red) * np.diag(sigma_red_200) * np.matrix(V_red[:num_data, :])
+# Compress - better resolution picture
+sigma_RED = np.zeros_like(sigma_red)
+sigma_GREEN = np.zeros_like(sigma_green)
+sigma_BLUE = np.zeros_like(sigma_blue)
 
-#green image
-sigma_green_200=np.zeros_like(sigma_g)
-sigma_green_200[:200] = sigma_g[:200]
-green_200 = np.matrix(U_green) * np.diag(sigma_green_200) * np.matrix(V_green[:num_data, :])
+# keeping the first 200 none zero elements as Sigma
+sigma_RED[0:200] = sigma_red[0:200]
+sigma_GREEN[0:200] = sigma_green[0:200]
+sigma_BLUE[0:200] = sigma_blue[0:200]
 
-#blue image
-sigma_blue_200=np.zeros_like(sigma_b)
-sigma_blue_200[:200] = sigma_b[:200]
-blue_200 = np.matrix(U_blue) * np.diag(sigma_blue_200) * np.matrix(V_blue[:num_data, :])
+RED   = U_red.dot(sigma_RED).dot(V_red)
+GREEN = U_green.dot(sigma_GREEN).dot(V_green)
+BLUE  = U_blue.dot(sigma_BLUE).dot(V_blue)
 
-#compress  the image into color image
-hi_img = img
-hi_img[:,:,0] = red_200
-hi_img[:,:,1] = green_200
-hi_img[:,:,2] = blue_200
+img1[:,:,0] = RED
+img1[:,:,1] = GREEN
+img1[:,:,2] = BLUE
 
-#display red, green, blue image.
-fig = plt.figure(1)
-ax1 = fig.add_subplot(2,2,1)
-ax2 = fig.add_subplot(2,2,2)
-ax3 = fig.add_subplot(2,2,3)
-ax4 = fig.add_subplot(2,2,4)
-ax1.imshow(red_200, cmap = 'Reds')
-ax2.imshow(green_200, cmap = 'Greens')
-ax3.imshow(blue_200, cmap = 'Blues')
-ax4.imshow(hi_img)
-fig = plt.gcf()
-fig.set_size_inches(15,15)
+figure = plt.figure(2)
+ax1 = figure.add_subplot(2,2,1)
+ax2 = figure.add_subplot(2,2,2)
+ax3 = figure.add_subplot(2,2,3)
+ax4 = figure.add_subplot(2,2,4)
+ax1.imshow(img1)
+ax2.imshow(RED, cmap = 'Reds')
+ax3.imshow(GREEN, cmap = 'Greens')
+ax4.imshow(BLUE, cmap = 'Blues')
 plt.suptitle('Higher Resolution, size=200')
 plt.show()
 
